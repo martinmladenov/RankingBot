880f786c98bd1adc6f60d699ddcc22179537a8ff
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-04-16 10:55:26 +0300

    Revert to displaying usernames as sometimes discord does not load mentions correctly

--- commands/ranks_command.py
+++ commands/ranks_command.py
@@ -10,7 +10,7 @@ class RanksCommand(commands.Cog):
 
     @commands.command()
     async def ranks(self, ctx):
-        rows = db_fetchall('SELECT user_id, rank, programme FROM ranks ORDER BY rank ASC')
+        rows = db_fetchall('SELECT username, rank, programme FROM ranks ORDER BY rank ASC')
 
         curr_programmes = set(map(lambda x: x[2], rows))
         grouped_ranks = [(p, [row for row in rows if row[2] == p]) for p in curr_programmes]
@@ -21,7 +21,7 @@ class RanksCommand(commands.Cog):
 
         for group in grouped_ranks:
             embed.add_field(name=f'**{group[0]}**',
-                            value=('\n'.join(('`' + (' ' * (3 - len(str(x[1])))) + str(x[1]) + '`  ' + f'<@{x[0]}>')
+                            value=('\n'.join(('`' + (' ' * (3 - len(str(x[1])))) + str(x[1]) + f' {x[0]}`')
                                              for x in group[1])),
                             inline=True)
 
