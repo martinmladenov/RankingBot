9d03e2642bdf1a0c9dbd8cce20b89bf79dd4b7fb
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-04-22 16:02:04 +0300

    Add setofferdate command to help

--- commands/helprank_command.py
+++ commands/helprank_command.py
@@ -14,4 +14,6 @@ class HelprankCommand(commands.Cog):
             f'`.ranks` Displays all ranks\n'
             f'`.clearrank <all/{"/".join(self.programmes)}>` Clears your rank for all programmes\n'
             f'`.helprank` Displays this message\n'
+            f'`.setofferdate <day> <month> <{"/".join(self.programmes)}>` Sets the date you\'ve received an offer '
+            'on Studielink to help other applicants predict when they might receive one\n'
         )
