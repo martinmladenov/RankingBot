99103060f26160c5603f5b1ef7f156528e167996
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-05-03 12:32:30 +0300

    Implement handle_awaiting_rank

--- utils/dm_util.py
+++ utils/dm_util.py
@@ -1,8 +1,9 @@
 import discord
 from enum import IntEnum
-from utils import programmes_util
+from utils import programmes_util, offer_date_util
 from database import db_fetchall, db_exec
-from datetime import datetime
+from datetime import datetime, date
+import re
 
 
 async def send_programme_rank_dm(member: discord.Member, programme: programmes_util.Programme):
@@ -52,6 +53,62 @@ async def send_programme_rank_dm(member: discord.Member, programme: programmes_u
                 (programme_id, DmStatus.AWAITING_RANK, datetime.utcnow(), str(user_id)))
 
     return True
+
+
+async def handle_awaiting_rank(message: discord.Message, dm_programme: str):
+    try:
+        match = re.search(r'^(\d+)[^A-Za-z0-9]+(\d+)[^A-Za-z0-9]+([A-Za-z0-9]+)$', message.content)
+
+        if match is None:
+            raise ValueError
+
+        parsed_rank = int(match.group(1))
+        parsed_date = offer_date_util.parse_offer_date(match.group(2), match.group(3))
+
+        if parsed_rank <= 0 or parsed_rank >= 10000 \
+                or parsed_rank <= programmes_util.programmes[dm_programme].places and parsed_date != date(2020, 4, 15):
+            await message.channel.send('Sorry, something seems wrong with the data you provided. '
+                                       'Please check your ranking number and date and use '
+                                       'the format `<rank> <day> <month>`.\n'
+                                       '_For example, if your ranking number is 100 and you\'ve received an offer on '
+                                       '15 April, type `100 15 April`._')
+            return False
+
+        rank_row = db_fetchall('SELECT rank FROM ranks WHERE user_id = %s AND programme = %s',
+                               (str(message.author.id), dm_programme))
+
+        if rank_row:
+            if rank_row[0][0] != parsed_rank:
+                await message.channel.send(f'It seems that you\'ve already set your ranking number '
+                                           f'to **{rank_row[0][0]}**. '
+                                           f'If that\'s incorrect, please type `.clearrank {dm_programme}`'
+                                           f'and reply with `{message.content}` again.')
+                return False
+
+            db_exec('DELETE FROM ranks WHERE user_id = %s AND programme = %s', (str(message.author.id), dm_programme))
+
+        db_exec('INSERT INTO ranks (user_id, username, rank, programme, offer_date) VALUES (%s, %s, %s, %s, %s)',
+                (message.author.id, message.author.name, parsed_rank, dm_programme, parsed_date))
+
+        db_exec('UPDATE user_data SET is_private = TRUE, dm_programme = NULL, dm_status = NULL '
+                'WHERE user_id = %s', (str(message.author.id),))
+
+        await message.channel.send('**Thank you for the information!**\n'
+                                   'We will do our best to put it to good use and help other applicants determine when '
+                                   'they might receive an offer.\n'
+                                   '_Note: Your username is now hidden from the statistics visible to '
+                                   'all server members. If you want it to be displayed, '
+                                   'you can type `.toggleprivaterank` on any channel on the server._')
+
+        return True
+    except ValueError:
+        await message.channel.send('Sorry, couldn\'t parse your response. Please use the following format: '
+                                   '`<rank> <day> <month>`.\n_For example, if your ranking number is '
+                                   '100 and you\'ve received an offer on 15 April, type `100 15 April`._')
+    except:
+        await message.channel.send('Sorry, an unexpected error occurred while processing your response.')
+
+    return False
 
 
 class DmStatus(IntEnum):
