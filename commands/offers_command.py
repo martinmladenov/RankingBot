d76c5b9d42fb4960cd17ad25be9e7817ee57cb41
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-05-15 23:16:25 +0300

    Only round offer ranks when is_private is set to true

--- commands/offers_command.py
+++ commands/offers_command.py
@@ -10,28 +10,31 @@ class OffersCommand(commands.Cog):
 
     @commands.command()
     async def offers(self, ctx):
-        rows = db_fetchall('select r.programme, r.rank, d.offer_date '
+        rows = db_fetchall('select r.programme, r.rank, d.offer_date, ud.is_private '
                            'from (select programme, max(rank) as rank from ranks '
                            'where ranks.offer_date is not null '
                            'group by programme) as r '
                            'inner join ranks as d '
                            'on r.programme = d.programme and r.rank = d.rank '
                            'and d.offer_date is not null '
+                           'left join user_data ud on d.user_id = ud.user_id '
                            'order by d.offer_date desc')
 
         embed = discord.Embed(title="Highest known ranks with offers", color=0x36bee6)
 
         for offer in rows:
             programme = programmes_util.programmes[offer[0]]
-            rounded_rank = round_rank(offer[1])
+            rank = offer[1]
             date_str = offer_date_util.format_offer_date(offer[2])
+            is_private = offer[3] is True
 
             embed.add_field(name=f'**{programme.icon} {programme.uni_name} {programme.display_name}**',
-                            value=f'≈**{rounded_rank}** on {date_str}',
+                            value=f'**{(("≈" + str(round_rank(rank))) if is_private else str(rank))}** on {date_str}',
                             inline=True)
 
-        embed.add_field(name='_This data has been provided by server members. Ranking numbers have been rounded to '
-                             'the nearest multiple of 5 to help protect their privacy._',
+        embed.add_field(name='_This data has been provided by server members. Some ranking numbers (as indicated '
+                             'by **≈** in front of them) have been rounded to the nearest multiple of 5 '
+                             'to help protect users\' privacy._',
                         value='To view all commands, type `.help`\n'
                               'To add the date you\'ve received an offer, type '
                               f'`.setofferdate <day> <month> <{programmes_util.get_ids_string()}>`',
