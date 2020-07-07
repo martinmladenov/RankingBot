805746ff2257b887b650ff232f908e42f7cd3c3b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-07-07 22:07:51 +0300

    Use services in setrank_command

--- commands/setrank_command.py
+++ commands/setrank_command.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
+
+from services import ranks_service, user_data_service
+from services.errors.invalid_operation_error import InvalidOperationError
 from utils import programmes_util
-from datetime import date
 
 
 class SetrankCommand(commands.Cog):
@@ -10,28 +12,34 @@ class SetrankCommand(commands.Cog):
     @commands.command()
     async def setrank(self, ctx, rank_number: int, programme: str):
         user = ctx.message.author
+        user_id = str(user.id)
 
-        if rank_number <= 0 or rank_number >= 10000 or programme not in programmes_util.programmes:
-            raise commands.UserInputError
+        async with self.bot.db_conn.acquire() as connection:
+            ranks = ranks_service.RanksService(connection)
+            users = user_data_service.UserDataService(connection)
 
-        try:
-            user_data_row = await self.bot.db_conn.fetchrow('SELECT user_id FROM user_data WHERE user_id = $1',
-                                                            str(user.id))
+            tr = connection.transaction()
+            await tr.start()
 
-            if not user_data_row:
-                await self.bot.db_conn.execute('INSERT INTO user_data (user_id, username) VALUES ($1, $2)',
-                                               str(user.id), user.name)
+            try:
+                try:
+                    await ranks.add_rank(user_id, rank_number, programme)
+                except ValueError:
+                    raise commands.UserInputError
+                except InvalidOperationError:
+                    await ctx.send(user.mention + ' You have already set your ranking number. To set a different one, '
+                                                  f'clear it using `.clearrank {programme}` and try setting it again.')
+                    await tr.rollback()
+                    return
 
-            await self.bot.db_conn.execute(
-                'INSERT INTO ranks (user_id, rank, programme, offer_date) VALUES ($1, $2, $3, $4)',
-                str(user.id), rank_number, programme,
-                date(2020, 4, 15) if rank_number <= programmes_util.programmes[programme].places else None)
-            await ctx.send(user.mention + ' Rank set.')
-        except Exception:
-            await ctx.send(user.mention + ' Unable to set rank.'
-                                          ' If you have already set a rank, try clearing it using '
-                                          f'`.clearrank {programme}`')
-            # raise
+                await users.add_user(user_id, user.name)
+
+            except:
+                await tr.rollback()
+                raise
+
+            await tr.commit()
+        await ctx.send(user.mention + ' Rank set.')
 
     @setrank.error
     async def info_error(self, ctx, error):
