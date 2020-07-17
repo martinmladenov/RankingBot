0288471b81aa2888f0e4ff3f13b6cea4647b0f1d
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-07-17 09:52:10 +0300

    Extract embed group building logic to response_building_util

---
+++ utils/response_building_util.py
@@ -0,0 +1,18 @@
+import discord
+
+
+def build_embed_groups(embed: discord.Embed, groups: dict, inline: bool = True):
+    for key in groups.keys():
+        start = 0
+        curr_length = 0
+        curr_field = 1
+
+        for i in range(len(groups[key])):
+            curr_length += len(groups[key][i]) + 1
+
+            if i == len(groups[key]) - 1 or curr_length + len(groups[key][i + 1]) >= 1024:
+                field_name = key if curr_field == 1 else f'{key}-{curr_field}'
+                embed.add_field(name=field_name, value=('\n'.join(groups[key][start:i + 1])), inline=inline)
+                start = i + 1
+                curr_length = 0
+                curr_field += 1
