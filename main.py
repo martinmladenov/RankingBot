6ab427ba6ddd266205b6fb0c601f08bddb0a8f78
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-05 23:59:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,5 +30,70 @@ async def on_ready():
 async def set_up_db():
     bot.db_conn = await asyncpg.create_pool(dsn=os.environ['DATABASE_URL'] + '?sslmode=require')
 
+create table ranks
 
+(
+
+    id         serial primary key,
+
+    user_id    varchar(50),
+
+    rank       int         not null,
+
+    programme  varchar(15) not null,
+
+    offer_date DATE,
+
+    UNIQUE (user_id, programme)
+
+);
+
+create table user_data
+
+(
+
+    id         serial primary key unique,
+
+    user_id    varchar(50) unique not null,
+
+    username   varchar(50)        not null,
+
+    is_private boolean            not null default false,
+
+    dm_programme varchar(15),
+
+    dm_status int,
+
+    dm_last_sent timestamp
+
+);
+
+create table received_dms
+
+(
+
+    id        serial primary key,
+
+    user_id   varchar(50) not null,
+
+    message   varchar(400),
+
+    success   boolean,
+
+    timestamp timestamp
+
+);
+
+create table excluded_programmes
+
+(
+
+    id        serial primary key,
+
+    user_id   varchar(50) not null,
+
+    programme varchar(15) not null
+
+)
+    
 bot.run(os.environ['DISCORD_SECRET'])
