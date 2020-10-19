f5a927596be14d2402579a7083aaf1448e4e87f5
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-10-19 19:32:05 +0300

    Add source parameter in add_rank

--- services/ranks_service.py
+++ services/ranks_service.py
@@ -9,7 +9,8 @@ class RanksService:
     def __init__(self, db_conn):
         self.db_conn = db_conn
 
-    async def add_rank(self, rank: int, programme: str, user_id: str = None, offer_date: date = None):
+    async def add_rank(self, rank: int, programme: str, user_id: str = None, offer_date: date = None,
+                       source: str = None):
         if rank <= 0 or rank >= 10000 or programme not in programmes_helper.programmes:
             raise ValueError
 
@@ -26,8 +27,8 @@ class RanksService:
                 raise DateIncorrectError
 
         await self.db_conn.execute(
-            'INSERT INTO ranks (user_id, rank, programme, offer_date) VALUES ($1, $2, $3, $4)',
-            user_id, rank, programme, offer_date)
+            'INSERT INTO ranks (user_id, rank, programme, offer_date, source) VALUES ($1, $2, $3, $4, $5)',
+            user_id, rank, programme, offer_date, source)
 
     async def delete_rank(self, user_id: str, programme: str):
         if programme is None:
