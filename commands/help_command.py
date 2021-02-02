293a1891c10e1236fb262c5d7cce5f872198710a
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2021-02-02 21:39:54 +0200

    Update .setofferdate command to use years

--- commands/help_command.py
+++ commands/help_command.py
@@ -17,7 +17,7 @@ class HelpCommand(commands.Cog):
             f'Shows the last known ranks which have received an offer\n'
             f'`.clearrank <all/{programmes_helper.get_ids_string()}> <year>` '
             f'Clears your rank for the specified programme\n'
-            f'`.setofferdate <day> <month> <{programmes_helper.get_ids_string()}>` Sets the date you\'ve received'
+            f'`.setofferdate <day> <month> <{programmes_helper.get_ids_string()}> [year]` Sets the date you\'ve received'
             f' an offer on Studielink to help other applicants predict when they might receive one\n'
             f'`.toggleprivaterank <{programmes_helper.get_ids_string()}>` Toggles whether your rank is displayed on '
             f'`.ranks` for the specified programme\n'
