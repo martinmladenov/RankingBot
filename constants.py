1c4e0df7ae8dddd31bdaa53c37f4d34bfbad085d
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2021-04-14 22:13:11 +0300

    Update accepted message id

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 administrator_user_id = 403569083402158090
 
-accepted_channel_id = 539780235411980294
-accepted_message_id = 699757067702894855
+accepted_message_id = 831969829258395759
 
 current_year = 2020
