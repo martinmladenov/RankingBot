c4b56eb2bc3c581c2b3cae75f4a13d65e081a566
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2021-04-17 01:41:48 +0300

    Activate slash command support

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import asyncpg
 
 from discord.ext import commands
 import discord
+from discord_slash import SlashCommand
 import os
 
 bot = commands.Bot(command_prefix='.', help_command=None)
+slash = SlashCommand(bot, sync_commands=True)
 
 if __name__ == '__main__':
     dirs = ['commands', 'handlers']
