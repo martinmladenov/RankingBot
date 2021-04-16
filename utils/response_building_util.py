4af389c6abd41263ed47634aea9c0c5383b6f37b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2021-04-16 19:58:55 +0300

    Make parts roughly equal in length when splitting groups

--- utils/response_building_util.py
+++ utils/response_building_util.py
@@ -7,10 +7,16 @@ def build_embed_groups(embed: discord.Embed, groups: dict, inline: bool = True):
         curr_length = 0
         curr_field = 1
 
+        char_count = sum(len(groups[key][i]) + 1 for i in range(len(groups[key])))
+        max_chars = char_count // ((char_count // 1024) + 1)
+
         for i in range(len(groups[key])):
             curr_length += len(groups[key][i]) + 1
 
-            if i == len(groups[key]) - 1 or curr_length + len(groups[key][i + 1]) >= 1024:
+            if i < len(groups[key]) - 1:
+                next_length = curr_length + len(groups[key][i + 1])
+
+            if i == len(groups[key]) - 1 or next_length >= 1024 or curr_length > max_chars:
                 field_name = key
                 embed.add_field(name=field_name, value=('\n'.join(groups[key][start:i + 1])), inline=inline)
                 start = i + 1
