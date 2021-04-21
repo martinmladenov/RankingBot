f471d674be06d461040f43d97c9ee6de4f5a1ec9
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2021-04-21 14:43:21 +0300

    Add /contribute command

---
+++ commands/contribute_command.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from discord_slash import SlashContext
+from discord_slash.cog_ext import cog_slash as slash
+
+
+class ContributeCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash(name='contribute',
+           description='Display information about helping the development of the bot')
+    async def contribute(self, ctx: SlashContext):
+        await ctx.send('The bot is open-source. You can find the source code '
+                       '_[on its GitHub repository](https://github.com/martinmladenov/RankingBot/)_.\n'
+                       'Feel free to contribute by creating an issue or submitting a pull request!')
+
+
+def setup(bot):
+    bot.add_cog(ContributeCommand(bot))
