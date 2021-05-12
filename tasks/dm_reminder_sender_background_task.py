8b34a2663d8a63970b737f362b288e239362b5a5
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2021-05-12 17:14:38 +0200

    Add background task to send reminders automatically

---
+++ tasks/dm_reminder_sender_background_task.py
@@ -0,0 +1,22 @@
+from discord.ext import tasks, commands
+from services import dm_service
+
+
+class DmReminderSenderBackgroundTask(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.send_reminders.start()
+
+    @tasks.loop(minutes=30)
+    async def send_reminders(self):
+        async with (await self.bot.get_db_conn()).acquire() as connection:
+            dm = dm_service.DMService(connection)
+            await dm.send_all_reminder_dms(self.bot)
+
+    @send_reminders.before_loop
+    async def before_printer(self):
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    bot.add_cog(DmReminderSenderBackgroundTask(bot))
