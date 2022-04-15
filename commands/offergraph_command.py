5d0c172528a4718445b92af06f1eb48139664478
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2022-04-15 03:04:55 +0200

    Rename 'programme_id' option to 'programme'

--- commands/offergraph_command.py
+++ commands/offergraph_command.py
@@ -17,7 +17,7 @@ class OffergraphCommand(commands.Cog):
            description='Show a graph of ranking numbers and the dates when they received offers',
            options=[
                create_option(
-                   name='programme_id',
+                   name='programme',
                    description='Study programme',
                    option_type=command_option_type.STRING,
                    required=True,
@@ -37,7 +37,7 @@ class OffergraphCommand(commands.Cog):
                    required=False
                )
            ])
-    async def offergraph(self, ctx: SlashContext, programme_id: str, year: int = None, approx: bool = True):
+    async def offergraph(self, ctx: SlashContext, programme: str, year: int = None, approx: bool = True):
         if year is None:
             year = constants.current_year
 
@@ -47,7 +47,7 @@ class OffergraphCommand(commands.Cog):
         async with (await self.bot.get_db_conn()).acquire() as connection:
             offers = offers_service.OffersService(connection)
             try:
-                await offers.generate_graph(programmes_helper.programmes[programme_id], not approx, year)
+                await offers.generate_graph(programmes_helper.programmes[programme], not approx, year)
             except ValueError:
                 await ctx.send('This programme was not numerus fixus in ' + str(year))
                 return
